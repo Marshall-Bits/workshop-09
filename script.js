7d1b4da81724f864369573d1914a89bb7cea8252
marshall-bits <kz_@hotmail.es>
2023-09-17 10:27:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const myHeaders = new Headers();
+
+myHeaders.append("Content-Type", "application/json");
+
+
+const raw = (prompt) => {
+
+    return JSON.stringify({
+        "key": "",
+        "prompt": prompt,
+        "negative_prompt": null,
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "20",
+        "seed": null,
+        "guidance_scale": 7.5,
+        "safety_checker": "yes",
+        "multi_lingual": "no",
+        "panorama": "no",
+        "self_attention": "no",
+        "upscale": "no",
+        "embeddings_model": null,
+        "webhook": null,
+        "track_id": null
+    });
+}
+
+const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+const imageResult = document.getElementById('image-result');
+
+const callAPI = () => {
+    let prompt = document.getElementById('prompt').value;
+    imageResult.src = "https://media2.giphy.com/media/4EFt4UAegpqTy3nVce/giphy.gif?cid=ecf05e476i6ds27lz0dxhy89u1ykp6e0yyk6aczdk717zjhx&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+    fetch("https://stablediffusionapi.com/api/v3/text2img", {
+
+        method: 'POST',
+        headers: myHeaders,
+        body: raw(prompt),
+        redirect: 'follow'
+
+    })
+        .then(response => response.json())
+        .then(result => imageResult.src = result.output[0])
+        .catch(error => console.log('error', error));
+}
+
+document.getElementById('btn').addEventListener('click', callAPI)
