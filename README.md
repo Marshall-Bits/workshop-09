1b846c3bb85cbe56aa6c41056f646c4e64a90ffd
marshall-bits <kz_@hotmail.es>
2023-09-22 09:10:03 +0200

    add comments and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Introducción al desarrollo web
+
+Este es el repositorio del workshop realizado en Ironhack Barcelona.
+
+Aquí podrás encontrar el proyecto entero con algunos comentarios adicionales. 
+ 
+[Presentación](https://docs.google.com/presentation/d/1Ok3zswIAogS8CHfUfarGyOTRySUKWF62CJj-oGZFHs0/edit?usp=sharing)
+
+[Mi LinkedIn](https://www.linkedin.com/in/marcel-bosch-developer/)
+
+Espero que lo disfrutes!
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const raw = (prompt) => {
+// Esta función construye el cuerpo de la petición a la API de Stable Diffusion
+function buildRequestBody(prompt) {
     return JSON.stringify({
+        // Recuerda añadir tu API key!
         "key": "",
         "prompt": prompt,
         "negative_prompt": null,
@@ -20,22 +22,30 @@ const raw = (prompt) => {
     });
 }
 
+// Aquí guardamos el elemento HTML que muestra la imagen, en este caso un <img> que tiene la id "image-result"
 const imageResult = document.getElementById('image-result');
 
-const callAPI = () => {
+// Esta función será la que se llame cuando se haga click en el botón "Generar imagen"
+function callAPI() {
+    // Aquí guardamos el texto que la usuaria ha escrito en el campo de texto
     let prompt = document.getElementById('prompt').value;
+    // Cambiamos la imagen por un gif de carga (un gif con una barra de progreso)
     imageResult.src = "https://media2.giphy.com/media/4EFt4UAegpqTy3nVce/giphy.gif?cid=ecf05e476i6ds27lz0dxhy89u1ykp6e0yyk6aczdk717zjhx&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+    // Hacemos la petición a la API de Stable Diffusion
     fetch("https://stablediffusionapi.com/api/v3/text2img", {
 
         method: 'POST',
         headers: { "Content-Type": "application/json" },
-        body: raw(prompt),
+        // Llamamos a la función que construye el cuerpo de la petición
+        body: buildRequestBody(prompt),
         redirect: 'follow'
 
     })
         .then(response => response.json())
+        // Una vez la API nos devuelve la respuesta, cambiamos la imagen por la que nos ha devuelto la API
         .then(result => imageResult.src = result.output[0])
         .catch(error => console.log('error', error));
 }
 
+// Añadimos un listener al botón "Generar imagen" para que cuando se haga click se llame a la función callAPI
 document.getElementById('btn').addEventListener('click', callAPI)
